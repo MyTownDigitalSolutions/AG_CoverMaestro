a63b5e2594fa1d2b84d062882c89252f94a47e23
MytownDMIncorporated <211675931+MyTownDMIncorporated@users.noreply.github.com>
2026-02-12 18:53:06 -0600

    UsePORTenbv var in root entrypoint

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
+import os
 from app.main import app
-
 
 def main():
     """Local entrypoint parity for platforms that invoke `python main.py`."""
     import uvicorn
-
-    uvicorn.run(app, host="0.0.0.0", port=8080)
-
+    port = int(os.getenv("PORT", "8080"))
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
 if __name__ == "__main__":
     main()
